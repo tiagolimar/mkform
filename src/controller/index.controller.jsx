25ba7c226e6cdd82e3d67c9ecde6d3482f47444b
tiagolimar <tiagolima.r2@gmail.com>
2023-12-04 07:43:42 -0300

    criando componentes

---
+++ src/controller/index.controller.jsx
@@ -0,0 +1,50 @@
+import { Form } from "react-bootstrap";
+
+export const componentes = {
+    tituloDeSecao,
+    Campo,
+    check,
+}
+
+function Campo(campo, chave, id) {
+    const { titulo, valor, tipo_de_valor } = campo;
+    return (
+        <Form.Group key={id} className="mt-2">
+            <Form.Label sm={3} className="fw-bold">
+                {titulo}
+            </Form.Label>
+            <Form.Control
+                type={tipo_de_valor}
+                defaultValue={valor}
+            />
+        </Form.Group>
+    )}
+
+function tituloDeSecao(campo, chave, id) {
+    const { valor, tipo_de_valor } = campo;
+    return (
+        <Form.Group key={id} className="mt-2">
+            <Form.Control
+                type={tipo_de_valor}
+                defaultValue={valor}
+                style={{
+                    border: "none",
+                    paddingLeft: 0,
+                    fontWeight: "bold",
+                }}
+            />
+        </Form.Group>
+    )}
+
+function check(campo, chave, id) {
+    const { titulo, valor, tipo_de_valor } = campo;
+    return (
+        <Form.Check
+            key={id}
+            type={tipo_de_valor}
+            id={chave}
+            label={titulo}
+            defaultChecked={valor}
+        />
+    );
+}
