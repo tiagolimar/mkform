e0cfd9a9122ad228823a6421f51a6854205e91d3
tiagolimar <tiagolima.r2@gmail.com>
2023-12-08 17:57:37 -0300

    mode

--- src/components/Disciplinas/Ar Condicionado/AreaPrivativa.jsx
+++ src/components/Disciplinas/Ar Condicionado/AreaPrivativa.jsx
@@ -1,15 +1,16 @@
+const opcoes = [
+    "SUÍTES / QUARTOS SOCIAL",
+    "ESTAR ÍNTIMO",
+    "ESTAR /JANTAR",
+    "VARANDA",
+    "CLOSET",
+    "QUARTO SERVIÇO (30% DE DEMANDA)",
+    "COZINHA (30% DE DEMANDA)",
+    "GABINETE",
+    "SUÍTE CASAL",
+];
+
 function AreaPrivativa() {
-    const opcoes = [
-        "SUÍTES / QUARTOS SOCIAL",
-        "ESTAR ÍNTIMO",
-        "ESTAR /JANTAR",
-        "VARANDA",
-        "CLOSET",
-        "QUARTO SERVIÇO (30% DE DEMANDA)",
-        "COZINHA (30% DE DEMANDA)",
-        "GABINETE",
-        "SUÍTE CASAL",
-    ];
     return opcoes.map((opcao,id) => {return <div key={id}>{opcao}</div>})
 }
 
